4fb906e6b8ce90457109c42dbed29b1b7a4a1caa
chenmuyang <41365058+chenmuyang@users.noreply.github.com>
2018-12-21 16:21:33 +0800

    Add files via upload

---
+++ nameLoading.py
@@ -0,0 +1,30 @@
+name=['chen','wang','li']
+password='111'
+count=0
+with open(r'C:\Users\Administrator\Desktop\sss.txt','r') as namelist:
+    list=namelist.readlines()
+    list1=[]
+    for i in list:
+        list1.append(i.rstrip('\n'))
+    print(list1)
+    while count<3 :
+        _name=input("name:")
+        _password=input("password:")
+        if _name in list1 :
+            print("ni yi bei suoding")
+            break
+        elif _name in name and _password==password:
+            print("ni hao:",_name)
+            break
+        else:
+            count+=1
+    else:
+        print("ni yi bei suoding")
+        with open(r'C:\Users\Administrator\Desktop\sss.txt', 'w') as namelist:
+            list.append(_name)
+            for i in list:
+                namelist.write(i+"\n")
+
+
+
+
